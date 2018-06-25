b7b809d9f8eafd2585e1b36b74a151ddd9b7ac75
soakes84 <spencer.oakes84@gmail.com>
2018-06-25 14:53:13 -0400

    setting up api key

--- WeatherNow/Form1.cs
+++ WeatherNow/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,22 @@ namespace WeatherNow
 {
     public partial class Form1 : Form
     {
+        const string APPID = "";
+        string cityName = "Charleston";
         public Form1()
         {
             InitializeComponent();
+            getWeather();
+        }
+
+        void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q=Charleston&appid=542ffd081e67f4512b705f89d2a611b2");
+
+                var json = web.DownloadString(url);
+            }
         }
     }
 }
